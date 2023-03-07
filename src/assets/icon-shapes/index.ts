e6a5b06a5d8956779428122d8c95f8162f5bf0c9
kev <kevbs.jsx@gmail.com>
2023-03-07 15:15:37 -0500

    deploy test, assets added

---
+++ src/assets/icon-shapes/index.ts
@@ -0,0 +1,10 @@
+import * as circular from './circular.webp';
+import * as rounded from './rounded.webp';
+import * as square from './square.webp';
+
+  /* eslint-disable */
+export const iconShapes = {
+  circular: circular.default.src,
+  rounded: rounded.default.src,
+  square: square.default.src
+};
---
+++ src/assets/icon-styles/index.ts
@@ -0,0 +1,18 @@
+import * as clay from './clay.webp';
+import * as flat from './flat.webp';
+import * as gradient from './gradient.webp';
+import * as illustrated from './illustrated.webp';
+import * as metallic from './metallic.webp';
+import * as pixelated from './pixelated.webp';
+import * as polygon from './polygon.webp';
+
+  /* eslint-disable */
+export const iconStyles =  {
+  clay: clay.default.src,
+  flat: flat.default.src,
+  gradient: gradient.default.src,
+  illustrated: illustrated.default.src,
+  metallic: metallic.default.src,
+  pixelated: pixelated.default.src,
+  polygon: polygon.default.src,
+};
